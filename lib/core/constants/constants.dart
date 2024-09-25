a4d289a5e111a6f98c48666409089eb118af7da3
Stepz <nine.chaitavat@gmail.com>
2024-09-25 16:13:29 +0700

    init project

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,4 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = '1b2c9c518a1a4888b08894c7fa202227';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
